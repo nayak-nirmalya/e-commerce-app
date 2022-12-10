2810b3441e9a13073fcd50c33172958591a55e3b
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-10 20:24:23 +0530

    added functionality of reading and writing to file

--- controllers/products.js
+++ controllers/products.js
@@ -14,11 +14,12 @@ exports.postAddProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll()
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProduct: products.length > 0,
+  Product.fetchAll((products) => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProduct: products?.length > 0,
+    })
   })
 }
--- models/product.js
+++ models/product.js
@@ -1,4 +1,21 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
+
+const savePath = path.join(
+  path.dirname(__filename),
+  '..',
+  'data',
+  'products.json',
+)
+
+const getProductFromFile = (cb) => {
+  fs.readFile(savePath, (err, fileContent) => {
+    if (err) {
+      return cb([])
+    }
+    cb(JSON.parse(fileContent))
+  })
+}
 
 module.exports = class Product {
   constructor(_title) {
@@ -6,10 +23,15 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this)
+    getProductFromFile((products) => {
+      products.push(this)
+      fs.writeFile(savePath, JSON.stringify(products), (err) => {
+        console.log(err)
+      })
+    })
   }
 
-  static fetchAll() {
-    return products
+  static fetchAll(cb) {
+    getProductFromFile(cb)
   }
 }
