80d8df561e40b336b635a9f369cfc1d3338ed3b3
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-10 11:40:12 +0530

    added 404.pug

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const path = require('path');
+const path = require('path')
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
+const app = express()
 
-app.set('view engine', 'pug');
-app.set('views', 'views');
+app.set('view engine', 'pug')
+app.set('views', 'views')
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.routes);
-app.use(shopRoutes);
+app.use('/admin', adminData.routes)
+app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+  res.status(404).render('404')
+})
 
-app.listen(3000);
+app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,23 @@
-const path = require('path');
+const path = require('path')
 
-const express = require('express');
+const express = require('express')
 
-const rootDir = require('../util/path');
+const rootDir = require('../util/path')
 
-const router = express.Router();
+const router = express.Router()
 
-const products = [];
+const products = []
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-});
+  res.render('add-product', { pageTitle: 'Add Product' })
+})
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+  products.push({ title: req.body.title })
+  res.redirect('/')
+})
 
-exports.routes = router;
-exports.products = products;
+exports.routes = router
+exports.products = products
