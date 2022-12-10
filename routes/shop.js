afc319727ee8328fd171a3cd9dd55a37861451a6
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-10 11:22:16 +0530

    converted shop.html to .pug

--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,15 @@
-const path = require('path');
+const path = require('path')
 
-const express = require('express');
+const express = require('express')
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const rootDir = require('../util/path')
+const adminData = require('./admin')
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/', (req, res, next) => {
-  res.render('shop');
-});
+  const products = adminData.products
+  res.render('shop', { prods: products, docTitle: 'Shop' })
+})
 
-module.exports = router;
+module.exports = router
