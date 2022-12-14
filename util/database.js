b57e24213e0fae83c6de12e61bed017324f6fbde
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-14 12:41:06 +0530

    initialized sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2')
+const Sequelize = require('sequelize')
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-store',
-    password: 'nirmalya'
+const sequelize = new Sequelize('node-store', 'root', 'nirmalya', {
+  dialect: 'mysql',
+  host: 'localhost',
 })
 
-module.exports = pool.promise()
+module.exports = sequelize
