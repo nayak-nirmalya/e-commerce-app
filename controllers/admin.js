8fe06e2d8321dcb5e38895c27def1130ade37a54
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-14 13:33:08 +0530

    admin can update product details

--- controllers/admin.js
+++ controllers/admin.js
@@ -26,30 +26,36 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/')
   }
   const prodID = req.params.productID
-  Product.findByID(prodID, (prod) => {
-    if (!prod) {
-      return res.redirect('/')
-    }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: prod,
+  Product.findByPk(prodID)
+    .then((prod) => {
+      if (!prod) {
+        return res.redirect('/')
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: prod,
+      })
     })
-  })
+    .catch((err) => console.error(err))
 }
 
 exports.postEditProduct = (req, res, next) => {
   const { productID, title, price, imageURL, description } = req.body
-  const updatedProduct = new Product(
-    productID,
-    title,
-    imageURL,
-    price,
-    description,
-  )
-  updatedProduct.save()
-  res.redirect('/admin/products')
+  Product.findByPk(productID)
+    .then((prod) => {
+      prod.title = title
+      prod.price = price
+      prod.imageURL = imageURL
+      prod.description = description
+      return prod.save()
+    })
+    .then((ret) => {
+      console.log('Updated Product!')
+      res.redirect('/admin/products')
+    })
+    .catch((err) => console.error(err))
 }
 
 exports.getProducts = (req, res, next) => {
