a3cc43c35649701f40b62e3c22906782d8ecc32d
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-14 18:01:28 +0530

    fetch cart products

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ Cart.belongsToMany(Product, { through: CartItem })
 Product.belongsToMany(Cart, { through: CartItem })
 
 sequelize
-  .sync({ force: true })
-  // .sync()
+  // .sync({ force: true })
+  .sync()
   .then((result) => {
     return User.findByPk(1)
   })
@@ -57,6 +57,9 @@ sequelize
     return user
   })
   .then((user) => {
+    user.createCart()
+  })
+  .then((cart) => {
     app.listen(3000)
   })
   .catch((err) => console.error(err))
--- controllers/shop.js
+++ controllers/shop.js
@@ -39,24 +39,21 @@ exports.getIndex = (req, res, next) => {
 }
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart((cart) => {
-    Product.findAll((products) => {
-      const cartProducts = []
-      for (product of products) {
-        const cartProductData = cart.products.find(
-          (prod) => prod.id === product.id,
-        )
-        if (cartProductData) {
-          cartProducts.push({ productData: product, qty: cartProductData.qty })
-        }
-      }
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: cartProducts,
-      })
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart
+        .getProducts()
+        .then((products) => {
+          res.render('shop/cart', {
+            path: '/cart',
+            pageTitle: 'Your Cart',
+            products: products,
+          })
+        })
+        .catch((err) => console.error(err))
     })
-  })
+    .catch((err) => console.error(err))
 }
 
 exports.postCart = (req, res, next) => {
